64e638edbc1125cbd8f07e6ae95226b2359d0014
cowilliams_deloitte <cowilliams@deloitte.com>
2024-10-31 14:36:06 -0600

    completed section 04

--- BANK/src/main.rs
+++ BANK/src/main.rs
@@ -14,6 +14,20 @@ impl Account {
         }
     }
 
+    fn summary(&self) -> String {
+        format!("Account Summary: Holder: {} has a balance of {}", self.holder, self.balance)
+    }
+
+    fn deposit (&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw (&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+
 }
 
 #[derive(Debug)]
@@ -25,32 +39,37 @@ impl Bank {
     fn new () -> Bank {
         Bank { accounts: vec![] }
     }
+
+    fn add_account (&mut self, account: Account) {
+        self.accounts.push(account)
+    }
+
+    fn total_balance (&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
+
 }
 
-fn print_account(account: &Account) {
-    println!("{:#?}", account);
-}
 
-fn print_num_accounts(bank: &Bank){
-    println!("Number of accounts: {}", bank.accounts.len());
-}
-
-fn change_account(account: &mut Account) {
-    account.balance = 10;
-
-    println!("{}", account.holder);
-}
-
-fn add_account (bank: &mut Bank, account: Account) {
-    bank.accounts.push(account)
-}
 
 fn main() {
     let mut bank = Bank::new();
-    let account = Account::new(1, String::from("Leo"));
-
-    add_account(&mut bank, account);
+    let mut account = Account::new(1, String::from("me"));
+    let mut account2 = Account::new(2, String::from("steve"));
     
-
-   println!("{:#?}, {:#?}", bank, bank);
+    account.deposit(10500);
+    account.withdraw(250);
+    account2.deposit(123303);
+    account2.withdraw(250);
+  
+    bank.add_account(account);
+    bank.add_account(account2);
+    println!("{:#?}, {}", bank.summary(), bank.total_balance());
 }
